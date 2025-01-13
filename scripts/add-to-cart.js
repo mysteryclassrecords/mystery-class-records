c2582aa94cc0e1ffdf8ec7627fad380c2ab5157b
mysteryclassrecords <mysteryclassrecords@gmail.com>
2025-01-13 00:32:45 -0600

    product page rough draft

---
+++ scripts/add-to-cart.js
@@ -0,0 +1,52 @@
+(function () {
+    const buttonContainer = document.querySelector('.product-buy-button');
+    const productId = buttonContainer.getAttribute('data-product-id'); // Get product ID
+  
+    var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+    if (window.ShopifyBuy) {
+      if (window.ShopifyBuy.UI) {
+        ShopifyBuyInit();
+      } else {
+        loadScript();
+      }
+    } else {
+      loadScript();
+    }
+  
+    function loadScript() {
+      var script = document.createElement('script');
+      script.async = true;
+      script.src = scriptURL;
+      (document.head || document.body).appendChild(script);
+      script.onload = ShopifyBuyInit;
+    }
+  
+    function ShopifyBuyInit() {
+      var client = ShopifyBuy.buildClient({
+        domain: '3eyau1-rk.myshopify.com',
+        storefrontAccessToken: '6dbf61b263e46db8a392d40ccdbf8901',
+      });
+  
+      ShopifyBuy.UI.onReady(client).then(function (ui) {
+        ui.createComponent('product', {
+          id: productId, // Use the dynamic product ID
+          node: document.getElementById('product-add-to-cart'),
+          options: {
+            product: {
+              contents: {
+                button: true,
+                imgWithCarousel: false,
+                img: false,
+                title: false,
+                description: false,
+                price: false,
+                options: false,
+              },
+              layout: 'vertical',
+            },
+          },
+        });
+      });
+    }
+  })();
+  
