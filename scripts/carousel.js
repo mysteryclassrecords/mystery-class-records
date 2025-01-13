9a58ba2a37cf9b253bb1b7496bd495a04377aeb7
mysteryclassrecords <mysteryclassrecords@gmail.com>
2025-01-13 01:33:54 -0600

    stylized text

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,52 +1,51 @@
 (function () {
-    const carouselContainer = document.querySelector('.product-carousel');
-    const productId = carouselContainer.getAttribute('data-product-id'); // Get product ID
-  
-    var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
-    if (window.ShopifyBuy) {
-      if (window.ShopifyBuy.UI) {
-        ShopifyBuyInit();
-      } else {
-        loadScript();
-      }
+  const carouselContainer = document.querySelector('.product-carousel');
+  const productId = carouselContainer.getAttribute('data-product-id'); // Get product ID
+
+  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
     } else {
       loadScript();
     }
-  
-    function loadScript() {
-      var script = document.createElement('script');
-      script.async = true;
-      script.src = scriptURL;
-      (document.head || document.body).appendChild(script);
-      script.onload = ShopifyBuyInit;
-    }
-  
-    function ShopifyBuyInit() {
-      var client = ShopifyBuy.buildClient({
-        domain: '3eyau1-rk.myshopify.com',
-        storefrontAccessToken: '6dbf61b263e46db8a392d40ccdbf8901',
-      });
-  
-      ShopifyBuy.UI.onReady(client).then(function (ui) {
-        ui.createComponent('product', {
-          id: productId, // Use the dynamic product ID
-          node: document.getElementById('product-image-carousel'),
-          options: {
-            product: {
-              contents: {
-                imgWithCarousel: true,
-                img: false,
-                button: false,
-                title: false,
-                description: false,
-                price: false,
-                options: false,
-              },
-              layout: 'vertical',
+  } else {
+    loadScript();
+  }
+
+  function loadScript() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = scriptURL;
+    (document.head || document.body).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: '3eyau1-rk.myshopify.com',
+      storefrontAccessToken: '6dbf61b263e46db8a392d40ccdbf8901',
+    });
+
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent('product', {
+        id: productId, // Use the dynamic product ID
+        node: document.getElementById('product-image-carousel'),
+        options: {
+          product: {
+            contents: {
+              imgWithCarousel: true,
+              img: false,
+              button: false,
+              title: false,
+              description: false,
+              price: false,
+              options: false,
             },
+            layout: 'vertical',
           },
-        });
+        },
       });
-    }
-  })();
-  
+    });
+  }
+})();
